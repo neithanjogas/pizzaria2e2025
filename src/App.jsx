a446fb9e4d86529572bf5a022bc3088190d76e67
neithanjogas <rm95268@estudante.fieb.edu.br>
2025-09-15 10:55:53 +0000

    estagio de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,16 @@
-import  { useState } from "react"
-function App(){
+import Produtos from "./pages/produtos"
 
-  const [nome, setNome] = useState('Ronaldo')
-  const [idade, setIdade] = useState(18)
-  const [time, setTime] = useState('pokemonas')
-  const [pi] = useState(3.14)
+const App = () => {
 
-  //var idade = 18;
-  //let time = 'pokemonas'
-  //const pi = 3.14;
-
-  const Formulario = (props)=> {
-    const [valor, setValor] = useState()
-   
-
-    return (
-      <div>
-        <input 
-  className= "nome"
-  placeholder={props.sombra}
-  onChange={(e)=>{setValor(e.target.value)}}
-  type="text" />
-
-
-  <button 
-    className="botao"
-    onClick={()=>{
-      if(valor.length > 0)
-        alert(valor);
-      else
-      alert(props.nome + 'idade é ' + props.idade)}}
-  >
-    CLIQUE AQUI
-  </button>
-        
-      </div>
-
-    )
-  }
-
-return (
-<div>
-  <h3>PIzzaria 2E</h3>
-
-    <Formulario nome="Ricardessobas"
-    idade={45}
-    sombra="Digite nome..." />
-    <Formulario nome="miliguidas" sombra="Digite sua idade..." />
-    <Formulario nome="venessarias" sombra="Digite seu time..." />
-    <Formulario nome="gustavaiusamariados" sombra="valor de pi" />
-
-</div>
-)
-
-
-
+  return (
+    <div>
+    <h1>Projeto Pizzaria</h1>
+    <Produtos>
+      
+       </Produtos>
+    
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,20 @@
+const Produtos = () => {
+   
+   
+    return(
+     <>
+        <h3>Listagem de Produtos</h3>
+    <ul>
+        <li>Pizza de Almoçarela</li>
+         <li>Pizza de Calabreso</li>
+         <li>Pizza opaivei</li>
+         <li>Pizza orapois</li>
+    </ul>
+        
+  </>
+
+
+    )
+}
+
+export default Produtos
